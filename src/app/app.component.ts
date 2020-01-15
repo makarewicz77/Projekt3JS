836bd8473bdfd354532c37d9cf9dbe3b726fe1f2
PaciekM <parfienczyk98@gmail.com>
2020-01-15 23:03:26 +0100

    Zrobiona tabelka i edycja pozycji + kasowanie.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component, ViewChild } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatTable } from '@angular/material';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+export interface UsersData {
+  id: number;
+  name: string;
+  surname: string;
+  pesel: number;
+}
+const ELEMENT_DATA: UsersData[] = [
+  {id: 1, name: 'Maciej', surname: 'Parfieńczyk', pesel:12345678910},
+  {id: 2, name: 'Maciej2',surname: 'Parfieńczyk2',pesel:12345678910},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +19,68 @@ import { Component, ViewChild } from '@angular/core';
 })
 
 export class AppComponent {
+  displayedColumns: string[] = ['id', 'name', 'surname','pesel','action'];
+  dataSource = ELEMENT_DATA;
+
+  @ViewChild(MatTable,{static:true}) table: MatTable<any>;
+  
+  id:number;
+  author:string ='';
+ 
+  constructor(private httpClient:HttpClient,public dialog: MatDialog){
+  }
+  openDialog(action,obj) {
+    obj.action = action;
+    const dialogRef = this.dialog.open(DialogBoxComponent, {
+      width: '250px',
+      data:obj
+    });
+ 
+    dialogRef.afterClosed().subscribe(result => {
+      if(result.event == 'Add'){
+        this.addRowData(result.data);
+      }else if(result.event == 'Update'){
+        this.updateRowData(result.data);
+      }else if(result.event == 'Delete'){
+        this.deleteRowData(result.data);
+      }
+    });
+  }
+  addRowData(row_obj){
+    var d = new Date();
+    this.dataSource.push({
+      id:row_obj.id,
+      name:row_obj.name,
+      surname:row_obj.surname,
+      pesel:row_obj.pesel,
+    });
+    this.table.renderRows();
+  }
+    updateRowData(row_obj){
+      this.dataSource = this.dataSource.filter((value,key)=>{
+        if(value.id == row_obj.id){
+          value.name = row_obj.name;
+          value.surname = row_obj.surname;
+          value.pesel = row_obj.pesel;
+        }
+        return true;
+      });
+    }
+    deleteRowData(row_obj){
+      this.dataSource = this.dataSource.filter((value,key)=>{
+        return value.id != row_obj.id;
+      });
+    }
+
+  getProfile()
+  {
+    this.httpClient.get('http://localhost:3000/posts')
+    .subscribe(
+      (data:any[]) =>
+        {
+          this.author = data[1].author;
+          alert(this.author);
+        });
+  }
   title = 'Projekt3JS';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule, MatButtonModule, MatIconModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,MatFormFieldModule, } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
+import { HttpClientModule } from '@angular/common/http';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
 // Some imports for menu modules
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogBoxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatTableModule,
+    MatFormFieldModule,
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    HttpClientModule
+  ],
+  entryComponents :
+  [
+    DialogBoxComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
