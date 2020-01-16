faa5a4a2c0b01a663b8a6de1878dd2f35dd6e106
PaciekM <parfienczyk98@gmail.com>
2020-01-16 17:28:48 +0100

    CRUD tabelka śmiga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,16 @@ import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatTable } from '@angular/material';
 import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 export interface UsersData {
   id: number;
   name: string;
   surname: string;
   pesel: number;
 }
-const ELEMENT_DATA: UsersData[] = [
-  {id: 1, name: 'Maciej', surname: 'Parfieńczyk', pesel:12345678910},
-  {id: 2, name: 'Maciej2',surname: 'Parfieńczyk2',pesel:12345678910},
-];
+
+//const ELEMENT_DATA: UsersData[] = getProfile();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,22 +20,29 @@ const ELEMENT_DATA: UsersData[] = [
 
 export class AppComponent {
   displayedColumns: string[] = ['id', 'name', 'surname','pesel','action'];
-  dataSource = ELEMENT_DATA;
+  dataSource : Observable<Array<UsersData>>;
 
   @ViewChild(MatTable,{static:true}) table: MatTable<any>;
   
   id:number;
   author:string ='';
  
-  constructor(private httpClient:HttpClient,public dialog: MatDialog){
+  constructor(private httpClient:HttpClient,public dialog: MatDialog)
+  {
+    this.dataSource = this.getProfiles();
+    console.log(this.dataSource);
   }
+  getProfiles(): Observable<any>{
+    return this.httpClient.get('http://localhost:3000/profile')
+     }
+
   openDialog(action,obj) {
     obj.action = action;
     const dialogRef = this.dialog.open(DialogBoxComponent, {
       width: '250px',
       data:obj
-    });
- 
+    })
+    
     dialogRef.afterClosed().subscribe(result => {
       if(result.event == 'Add'){
         this.addRowData(result.data);
@@ -46,30 +53,48 @@ export class AppComponent {
       }
     });
   }
-  addRowData(row_obj){
-    var d = new Date();
-    this.dataSource.push({
-      id:row_obj.id,
-      name:row_obj.name,
-      surname:row_obj.surname,
-      pesel:row_obj.pesel,
-    });
-    this.table.renderRows();
+  addRowData(row_obj)
+  {
+    this.httpClient.post<UsersData>('http://localhost:3000/profile',
+    {  
+    id:row_obj.id,
+    name:row_obj.name,
+    surname:row_obj.surname,
+    pesel:row_obj.pesel
+    })
+    .subscribe(
+      {
+        complete: () =>
+        this.dataSource = this.getProfiles()
+      }
+    );
+   
   }
     updateRowData(row_obj){
-      this.dataSource = this.dataSource.filter((value,key)=>{
-        if(value.id == row_obj.id){
-          value.name = row_obj.name;
-          value.surname = row_obj.surname;
-          value.pesel = row_obj.pesel;
+      this.httpClient.put('http://localhost:3000/profile/'+row_obj.id,
+      {  
+        id:row_obj.id,
+        name:row_obj.name,
+        surname:row_obj.surname,
+        pesel:row_obj.pesel
         }
-        return true;
-      });
+      )
+      .subscribe(
+        {
+        complete: () =>
+        this.dataSource = this.getProfiles()
+        }
+      );
     }
     deleteRowData(row_obj){
-      this.dataSource = this.dataSource.filter((value,key)=>{
-        return value.id != row_obj.id;
-      });
+      const url = 'http://localhost:3000/profile/'+row_obj.id
+      this.httpClient.delete(url)
+      .subscribe(
+        {
+        complete: () =>
+        this.dataSource = this.getProfiles()
+        }
+      );
     }
 
   getProfile()
